5cc7186ee5445382ebc097d18095705a13f75796
cerra-youcef <cerrafr31@gmail.com>
2025-02-11 09:59:24 +0100

    rajouter les deux api's config, timetable, v2

--- api/config/config.go
+++ api/config/config.go
@@ -1,25 +1,35 @@
-package timetable
+package config
 
 import (
 	"net/http"
 
-	"middleware/example/internal/controllers/EventsControllers"
+	"middleware/example/internal/controllers/AlertsControllers"
+	"middleware/example/internal/controllers/RessourcesControllers"
 	"github.com/go-chi/chi/v5"
 )
 
 func SetupRoutes(r chi.Router) {
-	// Routes pour les événements (events)
-	r.Route("/events", func(r chi.Router) {
-		r.Get("/", EventsControllers.GetAllEventsHandler) // GET /timetable/events
-		r.Post("/", EventsControllers.CreateEventHandler) // POST /timetable/events
+	// Routes pour les ressources (resources)
+	r.Route("/resources", func(r chi.Router) {
+		r.Get("/", RessourcesControllers.GetAllResourcesHandler) // GET /config/resources
+		r.Post("/", RessourcesControllers.CreateResourceHandler)  // POST /config/resources
 		r.Route("/{id}", func(r chi.Router) {
-			r.Use(EventsControllers.Ctx("eventId")) // Middleware pour injecter l'ID dans le contexte
-			r.Get("/", EventsControllers.GetEventByIDHandler)    // GET /timetable/events/{id}
-			r.Put("/", EventsControllers.UpdateEventHandler)     // PUT /timetable/events/{id}
-			r.Delete("/", EventsControllers.DeleteEventHandler)   // DELETE /timetable/events/{id}
+			r.Use(RessourcesControllers.Ctx("resourceId")) // Middleware pour injecter l'ID dans le contexte
+			r.Get("/", RessourcesControllers.GetResourceByIDHandler)    // GET /config/resources/{id}
+			r.Put("/", RessourcesControllers.UpdateResourceHandler)     // PUT /config/resources/{id}
+			r.Delete("/", RessourcesControllers.DeleteResourceHandler)   // DELETE /config/resources/{id}
 		})
 	})
 
-	// Route pour récupérer les événements par resource ID
-	r.Get("/events/resources/{resourceId}", EventsControllers.GetEventsByResourceIDHandler) // GET /timetable/events/resources/{resourceId}
+	// Routes pour les alertes (alerts)
+	r.Route("/alerts", func(r chi.Router) {
+		r.Get("/", AlertsControllers.GetAllAlertsHandler) // GET /config/alerts
+		r.Post("/", AlertsControllers.CreateAlertHandler)  // POST /config/alerts
+		r.Route("/{id}", func(r chi.Router) {
+			r.Use(AlertsControllers.Ctx("alertId")) // Middleware pour injecter l'ID dans le contexte
+			r.Get("/", AlertsControllers.GetAlertByIDHandler)    // GET /config/alerts/{id}
+			r.Put("/", AlertsControllers.UpdateAlertHandler)     // PUT /config/alerts/{id}
+			r.Delete("/", AlertsControllers.DeleteAlertHandler)   // DELETE /config/alerts/{id}
+		})
+	})
 }
