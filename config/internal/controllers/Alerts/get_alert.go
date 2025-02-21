170661428505fde1612db0e0ff9dc0834e5de2af
DonYanis <beldjilaliyanis@gmail.com>
2025-02-21 11:42:46 +0100

    fix get alert

--- config/internal/controllers/Alerts/get_alert.go
+++ config/internal/controllers/Alerts/get_alert.go
@@ -2,38 +2,44 @@ package Alerts
 
 import (
 	"encoding/json"
-	"github.com/go-chi/chi/v5"
 	"github.com/sirupsen/logrus"
 
 	"config/internal/services/Alerts"
 	"github.com/google/uuid"
-
 
 	"net/http"
 )
 
 // GetAlertByIDHandler gère la récupération d'une alerte par son ID.
 func GetAlertByIDHandler(w http.ResponseWriter, r *http.Request) {
-idParam := chi.URLParam(r, "id")
-id, err := uuid.Parse(idParam)
-if err != nil {
-http.Error(w, "Invalid UUID", http.StatusBadRequest)
-return
-}
 
-alert, err := Alerts.GetAlertByID(id)
-if err != nil {
-logrus.Errorf("Error retrieving alert: %v", err)
-http.Error(w, "Internal server error", http.StatusInternalServerError)
-return
-}
+	alertIdRaw := r.Context().Value("alertId") 
+	if alertIdRaw == nil {
+		logrus.Errorf("Alert ID not found in context")
+		http.Error(w, "Missing alert in context", http.StatusBadRequest)
+		return
+	}
 
-if alert == nil {
-http.Error(w, "Alert not found", http.StatusNotFound)
-return
-}
+	alertId, ok := alertIdRaw.(uuid.UUID)
+	if !ok {
+		logrus.Errorf("Alert ID is not of type uuid.UUID")
+		http.Error(w, "Invalid alert ID type", http.StatusBadRequest)
+		return
+	}
 
-w.Header().Set("Content-Type", "application/json")
-json.NewEncoder(w).Encode(alert)
+	alert, err := Alerts.GetAlertByID(alertId)
+	if err != nil {
+		logrus.Errorf("Error retrieving alert: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	if alert == nil {
+		http.Error(w, "Alert not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(alert)
 }
 
