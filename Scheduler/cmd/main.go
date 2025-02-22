e162125b9d8b6128209ea6c108b81ae96bba388c
DonYanis <beldjilaliyanis@gmail.com>
2025-02-22 13:19:06 +0100

    build scheduler

--- Scheduler/cmd/main.go
+++ Scheduler/cmd/main.go
@@ -1,30 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
-	"fmt"
-	"scheduler/internal/apiClients"
+	"scheduler/internal/nats"
+	"scheduler/internal/scheduler"
 )
 
 func main() {
-	configURL := "http://localhost:8080" 
-	
-	/*
-	resourceIDs := []int{13295, 13345} // Example resource IDs
-	events, err := apiClients.FetchEventsFromUCA(resourceIDs)
-	if err != nil {
-		fmt.Println("Error fetching events:", err)
-		return
-	}*/
+	log.Println("Scheduler Service Started")
 
+	// Initialize NATS connection
+	nats.InitNATS()
 
-	timetables, err := apiClients.FetchResourcesFromConfig(configURL)
-	if err != nil {
-		log.Fatalf("Error fetching timetables: %v", err)
-	}
-
-	// Convert to JSON and print
-	jsonData, _ := json.MarshalIndent(timetables, "", "  ")
-	fmt.Println(string(jsonData))
+	// Start scheduler
+	scheduler.StartScheduler()
 }
---
+++ Scheduler/internal/scheduler/scheduler.go
@@ -0,0 +1,58 @@
+package scheduler
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"scheduler/internal/apiClients"
+	"scheduler/internal/nats"
+
+	"github.com/zhashkevych/scheduler"
+)
+
+func FetchAndPublishEvents(ctx context.Context) {
+	configURL := "http://localhost:8080"
+
+	// Fetch resources
+	resources, err := apiClients.FetchResourcesFromConfig(configURL)
+	if err != nil {
+		log.Println("Error fetching resources:", err)
+		return
+	}
+
+	// Extract resource IDs
+	var resourceIDs []int
+	for _, r := range resources {
+		resourceIDs = append(resourceIDs, r.UcaId)
+	}
+
+	// Fetch events for these resources
+	events, err := apiClients.FetchEventsFromUCA(resourceIDs)
+	if err != nil {
+		log.Println("Error fetching events:", err)
+		return
+	}
+
+	// Publish events to NATS
+	for _, event := range events {
+		err := nats.PublishEvent("EVENTS.create", event)
+		if err != nil {
+			log.Println("Error publishing event:", err)
+		}
+	}
+
+	log.Println("Successfully fetched & published events")
+}
+
+// Start scheduler
+func StartScheduler() {
+	ctx := context.Background()
+	sc := scheduler.NewScheduler()
+	sc.Add(ctx, FetchAndPublishEvents, time.Minute*1) // Runs every 1 mins
+
+	// Keep program alive
+	quit := make(chan struct{})
+	<-quit
+	sc.Stop()
+}
